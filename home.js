d1882c82a43f5b4069f349431d6b437bd94d6046
Edaware Patricia <eedawarez@gmail.com>
2023-02-05 23:34:22 +0000

    Objects and the DOM

--- home.js
+++ home.js
@@ -5,16 +5,18 @@ console.log(productId);
 let myName = 'Pat';
 console.log(typeof productId);
 
-//object
-let person = {
-    firstName: 'John',
-    lastName: 'Adams'
-};
-
 let price = `Hello ${myName}`;
 showMessage(price.toUpperCase());
 console.log(price);
 console.log(price.substring(3));
+
+// object
+let person = {
+    firstName: 'John',
+    lastName: 'Adams',
+    age: 32,
+    partTime: false
+};
 
 // program flow
 if(productId > 50){
@@ -78,3 +80,73 @@ function getSecretCode2(value){
 }
 
 getSecretCode2(2);
+
+// Object and DOM - Document Object Model
+// Object parameters
+
+let mySymbol = Symbol();
+
+let person2 = {
+    firstName: 'Pat',
+    lastName: 'Zino',
+    age: 32,
+    partTime: false,
+    [mySymbol]: 'secretInformation',
+    showInfo: function(){
+        showMessage(this.lastName);
+    },
+    showInfo2: function(realAge){
+        showMessage(this.firstName + ' is ' + realAge);
+    }
+};
+
+console.log(person2.partTime);
+person2['partTime'] = true;
+console.log(person2.partTime);
+person2.showInfo();
+person2.showInfo2(24);
+
+// passing objects to functions
+
+function incrementAge(person2){
+    person2.age++;
+}
+
+incrementAge(person2);
+console.log(person2.age);
+
+// standard built-in objects -- https://developer.mozilla.org/en-US/docs/Web/JavaScript
+let now = new Date();
+console.log(now.toDateString());
+console.log(Math.random());
+console.log(Math.abs(-45));
+console.log(person2.firstName.charAt(2));
+let num = 234.455654645;
+console.log(num.toFixed(2));
+console.log(parseFloat(num.toString()));
+
+// Document Object Model
+const header = document.getElementById('message');
+header.style.color = 'green';
+header.style.fontWeight = '800';
+
+// detecting Button Clicks
+const button = document.getElementById('see-review');
+button.addEventListener('click', function(){
+    console.log('click');
+});
+
+// showing and hiding DOM elements
+const button2 = document.getElementById('see-review');
+
+button2.addEventListener('click', function(){   // this function can be used as a toggle
+    const review = document.getElementById('review');
+    if(review.classList.contains('d-none')){
+        review.classList.remove('d-none');
+        button2.textContent = 'CLOSE REVIEW';
+    }
+    else{
+        review.classList.add('d-none');
+        button2.textContent = 'SEE REVIEW';
+    }
+});
